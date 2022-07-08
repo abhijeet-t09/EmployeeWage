f50811d63eab3c3d57bbcb8b306078f76680a2e7
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-07-08 15:17:04 +0530

    Switch-Case Updated

--- src/com/BridgeLabz/EmployeeWage.java
+++ src/com/BridgeLabz/EmployeeWage.java
@@ -1,6 +1,5 @@
 package com.BridgeLabz;
 public class EmployeeWage {
-    //static final int EMPLOYEE_PRESENT=1;
     static final int WAGE_PER_HOUR=20;
     static final int FULL_DAY_HOUR=16;
     static final int PART_TIME_HOUR=8;
@@ -10,20 +9,24 @@ public class EmployeeWage {
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Program");
 
-        int employeePresent=(int)Math.floor(Math.random()*10)%3;
+        int empRandom=(int)Math.floor(Math.random()*10)%3;
         int dailyWage=0;
 
-        if(employeePresent==IS_FULL_TIME) {
-            System.out.println("Employee is Present!!");
-            dailyWage=WAGE_PER_HOUR * FULL_DAY_HOUR;
-        }
-        else if(employeePresent==IS_PART_TIME){
-            System.out.println("Employee works Part-time!!");
-            dailyWage=WAGE_PER_HOUR * PART_TIME_HOUR;
-        }
-        else
-            System.out.println("Employee is Absent!!");
+        switch(empRandom){
 
+            case IS_FULL_TIME:
+                System.out.println("Employee is Present!!");
+                dailyWage=WAGE_PER_HOUR * FULL_DAY_HOUR;
+                break;
+
+            case IS_PART_TIME:
+                System.out.println("Employee works Part-time!!");
+                dailyWage=WAGE_PER_HOUR * PART_TIME_HOUR;
+                break;
+
+            default:
+                System.out.println("Employee is Absent!!");
+        }
         System.out.println("Employee Daily Wage = Rs. "+dailyWage);
 
     }
