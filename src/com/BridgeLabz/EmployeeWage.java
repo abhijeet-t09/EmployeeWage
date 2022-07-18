8bcc8161f34d5d994d6be2b8a9b2da4c706fbb4e
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-07-18 15:23:29 +0530

    Multi-Company Wage Calculation Updated

--- src/com/BridgeLabz/EmployeeWage.java
+++ src/com/BridgeLabz/EmployeeWage.java
@@ -1,52 +1,56 @@
 package com.BridgeLabz;
 public class EmployeeWage {
-    static final int WAGE_PER_HOUR=20;
-    static final int FULL_DAY_HOUR=16;
-    static final int PART_TIME_HOUR=8;
     static final int IS_PART_TIME=1;
     static final int IS_FULL_TIME=2;
-    static final int  TOTAL_WORKING_HOURS=100;
-    static final int TOTAL_WORKING_DAYS=20;
-    static  int day=1;
-    static int workingHours=0;
-    static int totalWage=0;
+    //    static final int WAGE_PER_HOUR=20;
+    //    static final int FULL_DAY_HOUR=16;
+    //    static final int PART_TIME_HOUR=8;
+    //    static final int  TOTAL_WORKING_HOURS=100;
+    //    static final int TOTAL_WORKING_DAYS=20;
 
-    static void wageCalculation(){
 
-        while(day < TOTAL_WORKING_DAYS && workingHours < TOTAL_WORKING_HOURS) {
+    static void wageCalculation(String companyName, int wagePerHour, int totalWorkingDays, int totalWorkingHour){
+
+        int days=0;
+        int empHours=0;
+        int workingHours=0;
+        int totalWage=0;
+
+        while(days < totalWorkingDays && workingHours <= totalWorkingHour) {
             int empRandom = (int) Math.floor(Math.random() * 10) % 3;
             int dailyWage = 0;
 
             switch (empRandom) {
                 case IS_FULL_TIME:
                     System.out.println("Employee is Present!!");
-                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    workingHours+=FULL_DAY_HOUR;
+                    empHours=8;
                     break;
 
                 case IS_PART_TIME:
                     System.out.println("Employee works Part-time!!");
-                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                    workingHours+=PART_TIME_HOUR;
+                    empHours=4;
                     break;
 
                 default:
+                    empHours=0;
                     System.out.println("Employee is Absent!!");
 
             }
-            totalWage+=dailyWage;
-            System.out.println("Employee Daily Wage = Rs. " + dailyWage);
-            day++;
+            workingHours+=empHours;
+//            System.out.println("Employee Daily Wage = Rs. " + dailyWage);
+            days++;
         }
+        totalWage = workingHours * wagePerHour;
+        System.out.println();
+        System.out.println("Total Hours Worked: "+totalWorkingHour);
+        System.out.println("Total Days Worked: "+days);
+        System.out.println("Employee Monthly Wage = Rs. "+ totalWage);
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Program");
-
-        wageCalculation();
+        wageCalculation("Zydus Cadila", 50, 24, 80);
         System.out.println();
-        System.out.println("Total Hours Worked: "+workingHours);
-        System.out.println("Total Days Worked: "+day);
-        System.out.println("Employee Monthly Wage = Rs. "+ totalWage);
+        wageCalculation("Sun Pharma", 80, 20, 100);
     }
 }
